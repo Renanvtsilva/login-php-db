fdb52b86d085b39a857b2cde81758a32f943c57c
TecOp <craftland262@gmail.com>
2024-12-12 11:06:45 -0300

    criação do projeto

---
+++ public/index.php
@@ -0,0 +1,48 @@
+<?php
+
+// inicio da sessão
+
+session_start();
+
+// carregamento das rotas permitidas
+
+$rotas_permitidas = require_once __DIR__ . '/../inc/rotas.php';
+
+// definição das rota
+$rota = $_GET['rota'] ?? 'home';
+
+// verifica se o usuário esta logado
+if(!isset($_SESSION['usuario'])){
+    $rota = 'login';
+}
+
+// se o usuário esta logado e tenta entrar no login..
+if(isset($_SESSION['usuario']) && $rota === 'login'){
+    $rota = 'home';
+}
+
+// se a rota não existe
+if(!in_array($rota, $rotas_permitidas)){
+    $rota = '404';
+}
+
+// preparação da página
+$script = null;
+switch ($rota){
+    case '404':
+        $script = '404.php';
+        break;
+
+    case 'login':
+        $script = 'login.php';
+        break;
+
+    case 'home':
+            $script = 'home.php';
+            break;    
+}
+
+// apresentação da página
+require_once __DIR__ . "/../inc/header.php";
+require_once __DIR__ . "/../scripts/$script";
+require_once __DIR__ . "/../inc/footer.php";
